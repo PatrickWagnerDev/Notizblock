e1fef619bbc66822eff161d648565ef07c7dcbb8
Patrick Wagner <patrickwagner2607@gmail.com>
2026-09-09 12:21:20 +0200

    feat: clean Codebase

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-// To-Dos:
-
-// display notes
-// -> I need notes
 let notes = ["banana", "rasen mähen"];
+let trashNotes = [];
+let notesTitle = ['ba', 'Aufgabe'];
+let trashNotesTitle = [];
 
-// -> when should they be displayed?
 function renderNotes() {
-    // -> I need to define where they should be displayed
     let contentRef = document.getElementById('content');
     contentRef.innerHTML = "";
 
     for (let i = 0; i < notes.length; i++) {
         contentRef.innerHTML += getNoteTemplate(i);
     }
+}
 
+function renderTrashNotes() {
+    let trashContentRef = document.getElementById('trash_content');
+    trashContentRef.innerHTML = "";
+
+    for (let i = 0; i < trashNotes.length; i++) {
+        trashContentRef.innerHTML += getTrashNoteTemplate(i);
+    }
 }
 
 function getNoteTemplate(note) {
@@ -23,55 +28,28 @@ function getNoteTemplate(note) {
     `;
 }
 
-// add notes
-function addNote() {
-    // -> define user input
-    let noteInputRef = document.getElementById('note_input');
-
-    // -> read user input
-    let noteInput = noteInputRef.value;
-
-    // -> add input to notes
-    notes.push(noteInput);
-
-    // -> show input
-    renderNotes();
-
-    noteInputRef.value = "";
-}
-
-// delete notes
-function pushToArchiv(indexNote) {
-    // -> which note needs to be deleted?
-    let trashNote = notes.splice(indexNote, 1);
-    trashNotes.push(trashNote[0]);
-    let trashNoteTitle = notesTitle.splice(indexNote, 1);
-    trashNotesTitle.push(trashNoteTitle[0]);
-    // -> when should the note be deleted? -> done
-
-    // -> update the display
-    renderNotes();
-    renderTrashNotes();
-}
-
-// archive notes
-let trashNotes = [];
-
-function renderTrashNotes() {
-    let trashContentRef = document.getElementById('trash_content');
-    trashContentRef.innerHTML = "";
-
-    for (let i = 0; i < trashNotes.length; i++) {
-        trashContentRef.innerHTML += getTrashNoteTemplate(i);
-    }
-
-}
-
 function getTrashNoteTemplate(note) {
     return /*html*/`
         <h3>Title: ${trashNotesTitle[note]}</h3>
         <p>+ ${trashNotes[note]} <button onclick="deleteNote(${note})">X</button> <button onclick="recoverNote(${note})">+</button></p>
     `;
+}
+
+function addNote() {
+    let noteInputRef = document.getElementById('note_input');
+    let noteInput = noteInputRef.value;
+    notes.push(noteInput);
+    renderNotes();
+    noteInputRef.value = "";
+}
+
+function pushToArchiv(indexNote) {
+    let trashNote = notes.splice(indexNote, 1);
+    trashNotes.push(trashNote[0]);
+    let trashNoteTitle = notesTitle.splice(indexNote, 1);
+    trashNotesTitle.push(trashNoteTitle[0]);
+    renderNotes();
+    renderTrashNotes();
 }
 
 function deleteNote(indexNote) {
@@ -88,7 +66,3 @@ function recoverNote(indexNote) {
     renderNotes();
     renderTrashNotes();
 }
-
-// add Notes title
-let notesTitle = ['ba', 'Aufgabe'];
-let trashNotesTitle = [];
